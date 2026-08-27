715b50dbbd920a47100cc6efa96d58a0f02b8e72
Chandan janardan <chandanjanardan@gmail.com>
2026-08-27 17:45:53 +0530

    adding schma

--- src/modules/users/users.route.js
+++ src/modules/users/users.route.js
@@ -1,5 +1,6 @@
+const {userSchema} = require("./users.schema")
 function registerUser(app){
-    app.post("/user/register",async(request,reply)=>{
+    app.post("/user/register",{userSchema},async(request,reply)=>{
         const data = request.body
         return reply.code(201).send({user:data})
     })
---
+++ src/modules/users/users.schema.js
@@ -0,0 +1,15 @@
+const userSchema ={
+    schema:{
+        body:{
+            type:"object",
+            required:["email","name"],
+            properties:{
+                name:{type:"string",minLength:3},
+                age:{type:"integer"},
+                email:{type:"string",format:"email"}
+            }
+        }
+    }
+}
+
+module.exports={userSchema}
