8c0792d31a12f707435ffde24f34e9d469730d04
Chandan Janardan <chandanjanardan@gmail.com>
2026-08-29 00:16:25 +0530

    resolveing the merge confilic

--- src/modules/users/users.route.js
+++ src/modules/users/users.route.js
@@ -1,10 +1,6 @@
 const {userSchema} = require("./users.schema")
 function registerUser(app){
-<<<<<<< HEAD
-    app.post("/user/register",{userSchema},async(request,reply)=>{
-=======
     app.post("/user/register",userSchema,async(request,reply)=>{
->>>>>>> ed110d4 (ading agv and schama)
         const data = request.body
         return reply.code(201).send({user:data})
     })
